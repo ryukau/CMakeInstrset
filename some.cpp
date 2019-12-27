b4835385c80861aeadfa5f1cc372bd5652983dc4
cu <cu@localhost.localdomain>
2019-12-27 20:05:07 +0900

    Update

--- some.cpp
+++ some.cpp
@@ -4,13 +4,10 @@
 
 #if INSTRSET >= 10
 #define SOME_NAME Some_AVX512
-#warning Some_AVX512
 #elif INSTRSET >= 8
 #define SOME_NAME Some_AVX2
-#warning Some_AVX2
 #elif INSTRSET >= 5
 #define SOME_NAME Some_SSE41
-#warning Some_SSE41
 #else
 #error Unsupported instruction set
 #endif
