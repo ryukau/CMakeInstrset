e9de3f6df145f2375cdfe39a041f2029bab01bee
cu <cu@localhost.localdomain>
2019-12-27 16:09:10 +0900

    Update

--- test/run.sh
+++ test/run.sh
@@ -7,19 +7,21 @@
 # - clang++ : -O0 -O1 -Og
 #
 
-CXX=g++
+CXX=clang++
 OPT=-O3
 
-$CXX -std=c++17 $OPT -msse2 \
+echo Compiling some program.
+
+$CXX -v -std=c++17 $OPT -msse2 \
   -o some_sse2.o -c ../some.cpp
-$CXX -std=c++17 $OPT -msse4.1 \
+$CXX -v -std=c++17 $OPT -msse4.1 \
   -o some_sse41.o -c ../some.cpp
-$CXX -std=c++17 $OPT -mavx2 -mfma \
+$CXX -v -std=c++17 $OPT -mavx2 -mfma \
   -o some_avx2.o -c ../some.cpp
-$CXX -std=c++17 $OPT -mavx512f -mfma -mavx512vl -mavx512bw -mavx512dq \
+$CXX -v -std=c++17 $OPT -mavx512f -mfma -mavx512vl -mavx512bw -mavx512dq \
   -o some_avx512.o -c ../some.cpp
 
-$CXX -std=c++17 -O3 \
+$CXX -v -std=c++17 -O3 \
   some_sse2.o \
   some_sse41.o \
   some_avx2.o \
@@ -27,4 +29,7 @@ $CXX -std=c++17 -O3 \
   ../vcl/instrset_detect.cpp \
   ../main.cpp
 
+ls -la
+
+echo Executing some program.
 ./a.out
